0b83e746307d7b500d4571360c0f592bc88b1c65
Jerson <jersoncaibog0423@gmail.com>
2024-09-17 00:24:27 +0800

    all

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,5 @@
 import { UserProps } from "@/types";
-// import { ChakraProvider, Switch } from "@chakra-ui/react";
+import { ChakraProvider, Switch } from "@chakra-ui/react";
 import { signOut } from "next-auth/react";
 import { MdOutlineLogout } from "react-icons/md";
 
@@ -20,16 +20,16 @@ const Menu = ({
   };
 
   return (
-    // <ChakraProvider>
+    <ChakraProvider>
       <div className={`${isOpen ? "translate-y-[370px]" : ""} transition-all duration-[300ms] rounded-sm fixed right-2.5 top-[-300px] flex flex-col items-start h-fit w-fit min-w-[170px] bg-white p-3 z-[1000] `}>
         <div className="flex flex-col items-center w-full p-2 pb-4 pt-2 gap-3"> 
-          {/* <img 
+          <img 
             src={user?.image} 
             alt={`profile image`} 
             height={55}
             width={55}
             className="rounded-full"
-          /> */}
+          />
           <div className="flex flex-col items-center">
             <p className="font-semibold">{user?.name}</p>
             <p>{user?.email}</p>
@@ -37,7 +37,7 @@ const Menu = ({
         </div>
         <button className="py-3 px-2 w-full flex flex-row items-center justify-between border-y border-gray-300">
           Dark Mode
-          {/* <Switch size={"md"} className="outline-none" colorScheme="blue" onChange={setDarkMode} isChecked={darkMode} /> */}
+          <Switch size={"md"} className="outline-none" colorScheme="blue" onChange={setDarkMode} isChecked={darkMode} />
         </button>
         <button
           className="py-3 px-2 flex flex-row items-center gap-2 w-full active:bg-gray-100"
@@ -47,17 +47,9 @@ const Menu = ({
           Sign Out
         </button>
       </div>
-    // </ChakraProvider>
+    </ChakraProvider>
   );
 };
 
 export default Menu;
 
-
-// const Menu = () => {
-//   return (
-//     <div>Menu</div>
-//   )
-// }
-
-// export default Menu
