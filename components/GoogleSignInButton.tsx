7a95140bf6e30a5afc72097699f1d4b5d88432e2
Jerson <jersoncaibog0423@gmail.com>
2024-09-16 23:51:31 +0800

    all

--- components/GoogleSignInButton.tsx
+++ components/GoogleSignInButton.tsx
@@ -1,21 +1,20 @@
-'use client'
-import 'ldrs/ring'
+"use client";
 import googleLogo from "@/public/icons/google.png";
+import { ring2 } from "ldrs";
+import "ldrs/ring";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
-import { ring2 } from 'ldrs'
 
 const GoogleSignInButton = () => {
-
   // loading ui
-  ring2.register()
-  const [ loading, setLoading ] = useState(false)
+  ring2.register();
+  const [loading, setLoading] = useState(false);
 
   // sign in click handler
   const handleClick = () => {
-    setLoading(true)
-    signIn("google", { callbackUrl: '/chats' });
+    setLoading(true);
+    signIn("google", { callbackUrl: "/chats" });
   };
 
   return (
@@ -24,28 +23,27 @@ const GoogleSignInButton = () => {
         onClick={handleClick}
         className="bg-white p-3 min-w-[15rem] max-w-[24rem] w-[80vw] rounded-md flex flex-row items-center justify-center gap-3 active:bg-gray-100 font-semibold border border-gray-300 relative"
         // className="bg-white p-3 min-w-[15rem] max-w-[24rem] w-[80vw] rounded-lg flex flex-row items-center justify-center gap-3 active:bg-gray-100 font-semibold border border-gray-300"
-        >
-          {loading
-            ? (
-              <div className='absolute left-4 h0-full grid place-items-center'>
-                <l-ring-2
-                  size="22"
-                  stroke="2.5"
-                  stroke-length="0.25"
-                  bg-opacity="0.0"
-                  speed="0.8" 
-                  color="black" 
-                ></l-ring-2>
-              </div>
-            ) : (
-              <Image
-                src={googleLogo}
-                alt="Google Logo"
-                width={20}
-                height={20}
-                className="absolute left-4"
-              />
-          )}
+      >
+        {loading ? (
+          <div className="absolute left-4 h0-full grid place-items-center">
+            <l-ring-2
+              size="22"
+              stroke="2.5"
+              stroke-length="0.25"
+              bg-opacity="0.0"
+              speed="0.8"
+              color="black"
+            ></l-ring-2>
+          </div>
+        ) : (
+          <Image
+            src={googleLogo}
+            alt="Google Logo"
+            width={20}
+            height={20}
+            className="absolute left-4"
+          />
+        )}
         Continue with Google
       </button>
     </div>
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,5 @@
 import { UserProps } from "@/types";
-import { ChakraProvider, Switch } from "@chakra-ui/react";
+// import { ChakraProvider, Switch } from "@chakra-ui/react";
 import { signOut } from "next-auth/react";
 import { MdOutlineLogout } from "react-icons/md";
 
@@ -20,7 +20,7 @@ const Menu = ({
   };
 
   return (
-    <ChakraProvider>
+    // <ChakraProvider>
       <div className={`${isOpen ? "translate-y-[370px]" : ""} transition-all duration-[300ms] rounded-sm fixed right-2.5 top-[-300px] flex flex-col items-start h-fit w-fit min-w-[170px] bg-white p-3 z-[1000] `}>
         <div className="flex flex-col items-center w-full p-2 pb-4 pt-2 gap-3"> 
           {/* <img 
@@ -37,7 +37,7 @@ const Menu = ({
         </div>
         <button className="py-3 px-2 w-full flex flex-row items-center justify-between border-y border-gray-300">
           Dark Mode
-          <Switch size={"md"} className="outline-none" colorScheme="blue" onChange={setDarkMode} isChecked={darkMode} />
+          {/* <Switch size={"md"} className="outline-none" colorScheme="blue" onChange={setDarkMode} isChecked={darkMode} /> */}
         </button>
         <button
           className="py-3 px-2 flex flex-row items-center gap-2 w-full active:bg-gray-100"
@@ -47,7 +47,7 @@ const Menu = ({
           Sign Out
         </button>
       </div>
-    </ChakraProvider>
+    // </ChakraProvider>
   );
 };
 
