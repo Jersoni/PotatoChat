06374e6633f958f042979a77ee6c242edc1a67dd
Jerson <jersoncaibog0423@gmail.com>
2024-09-17 00:08:23 +0800

    all

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -1,175 +1,185 @@
-"use client";
-import { ChatWindow, Header, Messagebox } from "@/components";
-import supabase from "@/lib/supabaseClient";
-import { MessageProps, UserProps } from "@/types";
-import { useSession } from "next-auth/react";
-import { useParams } from "next/navigation";
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+// "use client";
+// import { ChatWindow, Header, Messagebox } from "@/components";
+// import supabase from "@/lib/supabaseClient";
+// import { MessageProps, UserProps } from "@/types";
+// import { useSession } from "next-auth/react";
+// import { useParams } from "next/navigation";
+// import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
-export default function Home() {
-  // params
-  const params = useParams();
+// export default function Home() {
+//   // params
+//   const params = useParams();
 
-  // dark mode
-  const [darkMode, setDarkMode] = useState(false);
+//   // dark mode
+//   const [darkMode, setDarkMode] = useState(false);
 
-  // user
-  const { data: session, status } = useSession();
-  const [recipient, setRecipient] = useState<UserProps>();
-  const [user, setUser] = useState<UserProps>();
+//   // user
+//   const { data: session, status } = useSession();
+//   const [recipient, setRecipient] = useState<UserProps>();
+//   const [user, setUser] = useState<UserProps>();
 
-  // get user data
-  useEffect(() => {
-    const fetchUser = async () => {
-      const { data, error } = await supabase
-        .from("users")
-        .select("*")
-        .eq("email", session?.user?.email);
+//   // get user data
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       const { data, error } = await supabase
+//         .from("users")
+//         .select("*")
+//         .eq("email", session?.user?.email);
 
-      if (error) {
-        console.log(error);
-      } else {
-        setUser({ ...data[0], ...session?.user });
-      }
-    };
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         setUser({ ...data[0], ...session?.user });
+//       }
+//     };
 
-    if (session?.user?.email) {
-      fetchUser();
-    }
-  }, [session]);
+//     if (session?.user?.email) {
+//       fetchUser();
+//     }
+//   }, [session]);
 
-  // get recipient data
-  useEffect(() => {
-    const getRecipientsData = async () => {
-      const { data, error } = await supabase
-        .from("users")
-        .select("*")
-        .eq("id", params.id);
+//   // get recipient data
+//   useEffect(() => {
+//     const getRecipientsData = async () => {
+//       const { data, error } = await supabase
+//         .from("users")
+//         .select("*")
+//         .eq("id", params.id);
 
-      if (error) {
-        console.log(error);
-      } else {
-        setRecipient(data[0]);
-      }
-    };
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         setRecipient(data[0]);
+//       }
+//     };
 
-    getRecipientsData();
-  }, [params]);
+//     getRecipientsData();
+//   }, [params]);
 
-  // message
-  const [message, setMessage] = useState("");
+//   // message
+//   const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    console.log(message);
-  }, [message]);
+//   useEffect(() => {
+//     console.log(message);
+//   }, [message]);
 
-  // messagebox change handler
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setMessage(e.target.value);
-  };
+//   // messagebox change handler
+//   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     setMessage(e.target.value);
+//   };
 
-  // messagebox submit handler
-  const MessageSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { error } = await supabase.from("messages").insert({
-      sender_id: user?.id,
-      message: message,
-      chat_id: convoID,
-    });
+//   // messagebox submit handler
+//   const MessageSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     const { error } = await supabase.from("messages").insert({
+//       sender_id: user?.id,
+//       message: message,
+//       chat_id: convoID,
+//     });
 
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("message sent successfully.");
-      setMessage("");
-    }
-  };
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("message sent successfully.");
+//       setMessage("");
+//     }
+//   };
 
-  // TODO: realtime subscription
-  // listen to database changes to the 'messages' table for INSERT events
-  const [messages, setMessages] = useState<MessageProps[]>([]);
-  useEffect(() => {
-    const subscribeToMessages = supabase
-      .channel("realtime:messages") // Create a channel for messages
-      .on(
-        "postgres_changes", // Listen to database changes
-        {
-          event: "INSERT",
-          schema: "public",
-          table: "messages",
-        },
-        (payload) => {
-          console.log("New message inserted:", payload.new);
-          setMessages((prevMessages) => [
-            ...prevMessages,
-            payload.new as MessageProps,
-          ]);
-        }
-      )
-      .subscribe();
+//   // TODO: realtime subscription
+//   // listen to database changes to the 'messages' table for INSERT events
+//   const [messages, setMessages] = useState<MessageProps[]>([]);
+//   useEffect(() => {
+//     const subscribeToMessages = supabase
+//       .channel("realtime:messages") // Create a channel for messages
+//       .on(
+//         "postgres_changes", // Listen to database changes
+//         {
+//           event: "INSERT",
+//           schema: "public",
+//           table: "messages",
+//         },
+//         (payload) => {
+//           console.log("New message inserted:", payload.new);
+//           setMessages((prevMessages) => [
+//             ...prevMessages,
+//             payload.new as MessageProps,
+//           ]);
+//         }
+//       )
+//       .subscribe();
 
-    // Cleanup the subscription when the component unmounts
-    return () => {
-      supabase.removeChannel(subscribeToMessages);
-    };
-  }, []);
+//     // Cleanup the subscription when the component unmounts
+//     return () => {
+//       supabase.removeChannel(subscribeToMessages);
+//     };
+//   }, []);
 
-  // fetch conversation
-  const [convoID, setConvoID] = useState<number>();
+//   // fetch conversation
+//   const [convoID, setConvoID] = useState<number>();
 
-  useEffect(() => {
-    const fetchConvo = async () => {
-      const { data, error } = await supabase
-        .from("chats")
-        .select()
-        .contains("participants", [user?.id, recipient?.id]);
+//   useEffect(() => {
+//     const fetchConvo = async () => {
+//       const { data, error } = await supabase
+//         .from("chats")
+//         .select()
+//         .contains("participants", [user?.id, recipient?.id]);
 
-      if (error) {
-        console.log(error);
-      } else {
-        setConvoID(data[0].id);
-      }
-    };
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         setConvoID(data[0].id);
+//       }
+//     };
 
-    if (user && recipient) {
-      fetchConvo();
-    }
-  }, [user, recipient]);
+//     if (user && recipient) {
+//       fetchConvo();
+//     }
+//   }, [user, recipient]);
 
-  // fetch all from table 'messages'
-  useEffect(() => {
-    const fetchMessages = async () => {
-      const { data, error } = await supabase
-        .from("messages")
-        .select("*")
-        .eq("chat_id", convoID);
+//   // fetch all from table 'messages'
+//   useEffect(() => {
+//     const fetchMessages = async () => {
+//       const { data, error } = await supabase
+//         .from("messages")
+//         .select("*")
+//         .eq("chat_id", convoID);
 
-      if (error) console.log(error);
-      else setMessages(data);
-    };
+//       if (error) console.log(error);
+//       else setMessages(data);
+//     };
 
-    if (convoID) {
-      fetchMessages();
-    }
-  }, [convoID]);
+//     if (convoID) {
+//       fetchMessages();
+//     }
+//   }, [convoID]);
 
+//   return (
+//     <div
+//       className={`${darkMode ? "bg-[#30343b]" : ""} 
+//       flex flex-col w-full`}
+//     >
+//       <Header
+//         user={user}
+//         recipient={recipient}
+//         darkMode={darkMode}
+//         setDarkMode={() => setDarkMode(!darkMode)}
+//       />
+//       <ChatWindow user={user} messages={messages} />
+//       <Messagebox
+//         onChange={onInputChange}
+//         message={message}
+//         onSubmit={MessageSubmitHandler}
+//       />
+//     </div>
+//   );
+// }
+
+import React from 'react'
+
+const Page = () => {
   return (
-    <div
-      className={`${darkMode ? "bg-[#30343b]" : ""} 
-      flex flex-col w-full`}
-    >
-      <Header
-        user={user}
-        recipient={recipient}
-        darkMode={darkMode}
-        setDarkMode={() => setDarkMode(!darkMode)}
-      />
-      <ChatWindow user={user} messages={messages} />
-      <Messagebox
-        onChange={onInputChange}
-        message={message}
-        onSubmit={MessageSubmitHandler}
-      />
-    </div>
-  );
+    <div>Page</div>
+  )
 }
+
+export default Page
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,54 +1,63 @@
-import { UserProps } from "@/types";
-// import { ChakraProvider, Switch } from "@chakra-ui/react";
-import { signOut } from "next-auth/react";
-import { MdOutlineLogout } from "react-icons/md";
+// import { UserProps } from "@/types";
+// // import { ChakraProvider, Switch } from "@chakra-ui/react";
+// import { signOut } from "next-auth/react";
+// import { MdOutlineLogout } from "react-icons/md";
 
-const Menu = ({
-    user,
-    isOpen,
-    darkMode,
-    setDarkMode
-} : {
-    user: UserProps | undefined
-    isOpen: boolean
-    darkMode: boolean
-    setDarkMode: () => void
-}) => {
+// const Menu = ({
+//     user,
+//     isOpen,
+//     darkMode,
+//     setDarkMode
+// } : {
+//     user: UserProps | undefined
+//     isOpen: boolean
+//     darkMode: boolean
+//     setDarkMode: () => void
+// }) => {
 
-  const handleSignOutClick = () => {
-    signOut({ callbackUrl: "/" });
-  };
+//   const handleSignOutClick = () => {
+//     signOut({ callbackUrl: "/" });
+//   };
 
+//   return (
+//     // <ChakraProvider>
+//       <div className={`${isOpen ? "translate-y-[370px]" : ""} transition-all duration-[300ms] rounded-sm fixed right-2.5 top-[-300px] flex flex-col items-start h-fit w-fit min-w-[170px] bg-white p-3 z-[1000] `}>
+//         <div className="flex flex-col items-center w-full p-2 pb-4 pt-2 gap-3"> 
+//           {/* <img 
+//             src={user?.image} 
+//             alt={`profile image`} 
+//             height={55}
+//             width={55}
+//             className="rounded-full"
+//           /> */}
+//           <div className="flex flex-col items-center">
+//             <p className="font-semibold">{user?.name}</p>
+//             <p>{user?.email}</p>
+//           </div>
+//         </div>
+//         <button className="py-3 px-2 w-full flex flex-row items-center justify-between border-y border-gray-300">
+//           Dark Mode
+//           {/* <Switch size={"md"} className="outline-none" colorScheme="blue" onChange={setDarkMode} isChecked={darkMode} /> */}
+//         </button>
+//         <button
+//           className="py-3 px-2 flex flex-row items-center gap-2 w-full active:bg-gray-100"
+//           onClick={handleSignOutClick}
+//         >
+//           <MdOutlineLogout size={20} />
+//           Sign Out
+//         </button>
+//       </div>
+//     // </ChakraProvider>
+//   );
+// };
+
+// export default Menu;
+
+
+const Menu = () => {
   return (
-    // <ChakraProvider>
-      <div className={`${isOpen ? "translate-y-[370px]" : ""} transition-all duration-[300ms] rounded-sm fixed right-2.5 top-[-300px] flex flex-col items-start h-fit w-fit min-w-[170px] bg-white p-3 z-[1000] `}>
-        <div className="flex flex-col items-center w-full p-2 pb-4 pt-2 gap-3"> 
-          {/* <img 
-            src={user?.image} 
-            alt={`profile image`} 
-            height={55}
-            width={55}
-            className="rounded-full"
-          /> */}
-          <div className="flex flex-col items-center">
-            <p className="font-semibold">{user?.name}</p>
-            <p>{user?.email}</p>
-          </div>
-        </div>
-        <button className="py-3 px-2 w-full flex flex-row items-center justify-between border-y border-gray-300">
-          Dark Mode
-          {/* <Switch size={"md"} className="outline-none" colorScheme="blue" onChange={setDarkMode} isChecked={darkMode} /> */}
-        </button>
-        <button
-          className="py-3 px-2 flex flex-row items-center gap-2 w-full active:bg-gray-100"
-          onClick={handleSignOutClick}
-        >
-          <MdOutlineLogout size={20} />
-          Sign Out
-        </button>
-      </div>
-    // </ChakraProvider>
-  );
-};
+    <div>Menu</div>
+  )
+}
 
-export default Menu;
+export default Menu
